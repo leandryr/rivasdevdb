b8849f0d2553a9f1ac28463aa345763c6c9ffa8b
Leandryr <rivasleandry@gmail.com>
2025-02-02 12:21:40 -0500

    Fix: Eliminación de opciones obsoletas de MongoDB

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 // Conexión a MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB conectado"))
   .catch((error) => console.error("❌ Error al conectar a MongoDB:", error));
 
